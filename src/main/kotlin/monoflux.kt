13b2f2e2e838c9e1d69e4bdef4c747ad8dfa5321
bosuksh <bosuksh@gmail.com>
2021-06-11 14:37:27 +0900

    feat: git test 브랜치도 진행된다-3

--- src/main/kotlin/monoflux.kt
+++ src/main/kotlin/monoflux.kt
@@ -16,7 +16,8 @@ fun main(args: Array<String>){
 
     println("git master는 진행된다 ")
     println("master는 계속 또 진행됨")
-    println("123123")
+    println("123123 from master")
+    println("456456 from test")
     println("789789")
 
 
